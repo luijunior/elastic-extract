0549826681a73b47732b2eadf968b4eb084ffcb9
Luiz Antonio <luizgj@ipiranga.com.br>
2017-10-05 10:27:37 -0300

    Mudando porta para deploy openshift

--- app.py
+++ app.py
@@ -103,5 +103,5 @@ def gera_string(data_inicio, data_fim):
 if __name__ == '__main__':
     app.secret_key = 'super-secret-key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
 
